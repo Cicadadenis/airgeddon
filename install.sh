3649905bc550ff91a06b1db4d6a1278b161bb716
Denis_Bednakov <50529176+bednakovdenis@users.noreply.github.com>
2020-04-14 18:19:13 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+sudo apt install figlet -y
+
+figlet Bednakov-Xack-Live
+
+sleep 3
+
+sudo apt install sslstrip -y
+
+sudo apt install asleap -y
+
+sudo apt install bettercap -y
+
+sudo apt install mdk4 -y
+
+sudo apt install hostapd -y
+
+sudo apt install beef-xss -y
+
+sudo apt install lighttpd -y
+
+chmod +x airgeddon.sh 
+
+./airgeddon.sh
